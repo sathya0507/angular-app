2db16eec4c4b62d0db736a8740c5eda7e6d34733
sathya <sathyamuthu0529@gmail.com>
2023-11-07 17:08:30 +0530

    concept: Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { PropercasePipe } from './shared/pipes/propercase.pipe';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, DataBindingComponent, ParentComponent, ChildComponent, TodoListComponent],
+  declarations: [AppComponent, TodoComponent, DataBindingComponent, ParentComponent, ChildComponent, TodoListComponent, PropercasePipe],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/shared/pipes/propercase.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PropercasePipe } from './propercase.pipe';
+
+describe('PropercasePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PropercasePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
