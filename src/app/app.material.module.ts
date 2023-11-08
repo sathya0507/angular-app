85e2b732c2917eb37c4078b44ddc68ee1d8cc4d3
sathya <sathyamuthu0529@gmail.com>
2023-11-08 17:32:27 +0530

    added angular-material

---
+++ src/app/app.material.module.ts
@@ -0,0 +1,9 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+export const AppModuleMaterials = [
+  MatButtonModule,
+  MatInputModule,
+  MatCheckboxModule,
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { FormsModule } from '@angular/forms';
 import { TodoComponent } from './todo/todo.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
@@ -11,10 +9,26 @@ import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { PropercasePipe } from './shared/pipes/propercase.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppModuleMaterials } from './app.material.module';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent, DataBindingComponent, ParentComponent, ChildComponent, TodoListComponent, PropercasePipe],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    DataBindingComponent,
+    ParentComponent,
+    ChildComponent,
+    TodoListComponent,
+    PropercasePipe,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    AppModuleMaterials,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
