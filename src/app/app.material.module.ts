b435dbff312a714fe4434095d50daad568f799ad
sathya <sathyamuthu0529@gmail.com>
2023-11-09 16:13:10 +0530

    initial commit

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -1,9 +1,11 @@
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export const AppModuleMaterials = [
   MatButtonModule,
   MatInputModule,
   MatCheckboxModule,
+  MatFormFieldModule,
 ];
